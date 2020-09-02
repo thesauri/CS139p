952725b64b57cee558450730d440c5a5c7dcfb0d
Walter Berggren <walter.berggren@gmail.com>
2020-09-03 00:17:13 +0300

    Initial Commit

---
+++ CS193p-memorization/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CS193p-memorization
+//
+//  Created by Walter Berggren on 2020-09-03.
+//  Copyright © 2020 Walter Berggren. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
