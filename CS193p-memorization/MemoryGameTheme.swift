616ab53f84f0e28c4b59f744abd06accf5311242
Walter Berggren <walter.berggren@gmail.com>
2020-09-13 13:34:26 +0200

    Add themes

--- CS193p-memorization/MemoryGameTheme.swift
+++ CS193p-memorization/MemoryGameTheme.swift
@@ -9,7 +9,12 @@
 import SwiftUI
 
 enum MemoryGameThemes: CaseIterable {
-    case faces
+    case mixedTheme
+    case happyFaces
+    case sadFaces
+    case fruits
+    case vehicles
+    case weapons
 
     static func randomTheme() -> MemoryGameTheme {
         let themeCase = MemoryGameThemes.allCases.randomElement()
@@ -18,8 +23,13 @@ enum MemoryGameThemes: CaseIterable {
 
     static func theme(for themeCase: MemoryGameThemes?) -> MemoryGameTheme {
         switch themeCase {
-            case .faces: return FacesTheme()
-            default: return FacesTheme()
+            case .mixedTheme: return MixedTheme()
+            case .happyFaces: return HappyFaces()
+            case .sadFaces: return SadFaces()
+            case .fruits: return Fruits()
+            case .vehicles: return Vehicles()
+            case .weapons: return Weapons()
+            default: return MixedTheme()
         }
     }
 }
@@ -31,9 +41,44 @@ protocol MemoryGameTheme {
     var numberOfPairsOfCards: Int? { get }
 }
 
-struct FacesTheme: MemoryGameTheme {
+struct MixedTheme: MemoryGameTheme {
     var name: String = "Faces"
     var emojis: [String] = ["🦠", "🌭", "🤓", "💨", "🇸🇪"]
     var color: Color = Color.yellow
+    var numberOfPairsOfCards: Int?
+}
+
+struct HappyFaces: MemoryGameTheme {
+    var name: String = "Happy Faces"
+    var emojis: [String] = ["😁", "🤓", "🙂", "😍", "😎"]
+    var color: Color = Color.yellow
     var numberOfPairsOfCards: Int? = 5
+}
+
+struct SadFaces: MemoryGameTheme {
+    var name: String = "Sad Faces"
+    var emojis: [String] = ["😔", "🙁", "😩", "🥺", "😭"]
+    var color: Color = Color.yellow
+    var numberOfPairsOfCards: Int? = 5
+}
+
+struct Fruits: MemoryGameTheme {
+    var name: String = "Fruits"
+    var emojis: [String] = ["🍎", "🍐", "🍆", "🍑", "🍌"]
+    var color: Color = Color.green
+    var numberOfPairsOfCards: Int?
+}
+
+struct Vehicles: MemoryGameTheme {
+    var name: String = "Vehicles"
+    var emojis: [String] = ["🚗", "✈️", "🚂", "🛺", "🚀"]
+    var color: Color = Color.green
+    var numberOfPairsOfCards: Int?
+}
+
+struct Weapons: MemoryGameTheme {
+    var name: String = "Weapons"
+    var emojis: [String] = ["💣", "⚔️", "🔫", "🔪", "💩"]
+    var color: Color = Color.yellow
+    var numberOfPairsOfCards: Int?
 }
