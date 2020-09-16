7ad4195c05297632956fa965e0d0737061bc487a
Walter Berggren <walter.berggren@gmail.com>
2020-09-16 23:43:58 +0300

    Create Set game project

---
+++ Set game/Set game/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Set game
+//
+//  Created by Walter Berggren on 2020-09-16.
+//  Copyright © 2020 Walter Berggren. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
